85006c2e6a9d2d04b7aebc5682a5ca0d76126f76
= <=>
2019-03-20 14:33:50 +0100

    PC

--- PMSUMail/app/src/main/java/com/example/pmsumail/SplashActivity.java
+++ PMSUMail/app/src/main/java/com/example/pmsumail/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.pmsumail;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,31 +21,37 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        Log.e("TAG", "SplashActivity --- > onStart");
         super.onStart();
     }
 
     @Override
     protected void onRestart() {
+        Log.e("TAG", "SplashActivity --- > onRestart");
         super.onRestart();
     }
 
     @Override
     protected void onResume() {
+        Log.e("TAG", "SplashActivity --- > onResume");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
+        Log.e("TAG", "SplashActivity --- > onPause");
         super.onPause();
     }
 
     @Override
     protected void onStop() {
+        Log.e("TAG", "SplashActivity --- > onStop");
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
+        Log.e("TAG", "SplashActivity --- > onDestroy");
         super.onDestroy();
     }
 }
